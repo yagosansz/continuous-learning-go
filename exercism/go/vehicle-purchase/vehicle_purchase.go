1457320f75d9d12c5f2efb7bbe1bd69ec2fc9ae9
Yago Santos <yagosansz@gmail.com>
2025-03-02 22:22:06 -0500

    feat: exercism go vehicle-purchase-palace solution

---
+++ exercism/go/vehicle-purchase/vehicle_purchase.go
@@ -0,0 +1,33 @@
+package purchase
+
+// NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
+func NeedsLicense(kind string) bool {
+	if kind == "car" || kind == "truck" {
+		return true
+	}
+	return false
+}
+
+// ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
+func ChooseVehicle(option1, option2 string) string {
+	var chosenOption string
+	if option1 < option2 {
+		chosenOption = option1
+	} else {
+		chosenOption = option2
+	}
+	return chosenOption + " is clearly the better choice."
+}
+
+// CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
+func CalculateResellPrice(originalPrice, age float64) float64 {
+	var resellPrice float64
+	if age >= 10 {
+		resellPrice = originalPrice * 0.5
+	} else if age >= 3 && age < 10 {
+		resellPrice = originalPrice * 0.7
+	} else {
+		resellPrice = originalPrice * 0.8
+	}
+	return resellPrice
+}
